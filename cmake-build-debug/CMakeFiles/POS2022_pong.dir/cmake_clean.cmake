dd12735380707d68f584b4849e8c393e7ded6f05
Samuel Pekar <samuel.pekar@m2ms.sk>
2022-12-28 13:17:34 +0100

    project init

---
+++ cmake-build-debug/CMakeFiles/POS2022_pong.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POS2022_pong.dir/main.cpp.o"
+  "POS2022_pong"
+  "POS2022_pong.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/POS2022_pong.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/POS2022_pong.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for POS2022_pong.
+# This may be replaced when dependencies are built.
