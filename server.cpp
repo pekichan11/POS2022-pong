a99c438bde8e1f69bd7594cf962729cdd341cde8
Samuel Pekar <samuel.pekar@m2ms.sk>
2022-12-30 12:44:57 +0100

    server init

---
+++ server.cpp
@@ -0,0 +1,57 @@
+#include <unistd.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "server.h"
+
+int server::spustiServer(int argc, char **argv) {
+    int sockfd, newsockfd;
+    socklen_t cli_len;
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    char buffer[256];
+
+    if (argc < 2)
+    {
+        fprintf(stderr,"usage %s port\n", argv[0]);
+        return 1;
+    }
+
+    bzero((char*)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(atoi(argv[1]));
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("Error creating socket");
+        return 1;
+    }
+
+    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        perror("Error binding socket address");
+        return 2;
+    }
+
+    listen(sockfd, 5);
+    cli_len = sizeof(cli_addr);
+
+    newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &cli_len);
+    if (newsockfd < 0)
+    {
+        perror("ERROR on accept");
+        return 3;
+    }
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ server.h
@@ -0,0 +1,13 @@
+
+#ifndef POS2022_PONG_SERVER_H
+#define POS2022_PONG_SERVER_H
+
+
+class server {
+public:
+    server() = default;
+    int spustiServer(int argc, char* argv[]);
+};
+
+
+#endif
