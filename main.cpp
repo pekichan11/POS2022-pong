00f50fbed993cc980e69d2191280d59cfbcae22a
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-01-06 19:14:24 +0100

    rozhodnovanie ci klient ci server

--- main.cpp
+++ main.cpp
@@ -4,14 +4,28 @@
 #include "FileHandling/FileRead.h"
 #include "FileHandling/FileWrite.h"
 #include "Game/Game.h"
+#include <SFML/Network.hpp>
+#include "server.h"
+#include "client.h"
 
-int main() {
+
+int main(int argc, char * argv[]) {
+
+    server server;
+    client client;
+    sf::TcpSocket* socket;
+    if(argv[1] != "c") {
+        socket = server.spustiServer(atoi(argv[2]));
+    } else {
+        socket = client.runTcpClient(atoi(argv[2]), argv[3]);
+    }
+
     std::mutex mutex;
     std::list<std::string> results = {};
     ThreadData data = {5, results};
 
     std::thread fileRead(FileRead(), &data, std::ref(mutex));
-    std::thread play(Game(), &data, std::ref(mutex));
+    std::thread play(Game(socket), &data, std::ref(mutex));
     std::thread fileWrite(FileWrite(), &data, std::ref(mutex));
 
     fileRead.join();
