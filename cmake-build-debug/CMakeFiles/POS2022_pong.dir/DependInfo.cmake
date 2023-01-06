79da7e5e5fa372e69ca4439df4c51391fd6888f4
Samuel Pekar <samuel.pekar@m2ms.sk>
2023-01-06 20:02:11 +0100

    cmake-build-debug

--- cmake-build-debug/CMakeFiles/POS2022_pong.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/POS2022_pong.dir/DependInfo.cmake
@@ -8,13 +8,14 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/cerve/CLionProjects/POS2022-pong/Ball/Ball.cpp" "CMakeFiles/POS2022_pong.dir/Ball/Ball.cpp.obj" "gcc" "CMakeFiles/POS2022_pong.dir/Ball/Ball.cpp.obj.d"
-  "C:/Users/cerve/CLionProjects/POS2022-pong/FileHandling/FileRead.cpp" "CMakeFiles/POS2022_pong.dir/FileHandling/FileRead.cpp.obj" "gcc" "CMakeFiles/POS2022_pong.dir/FileHandling/FileRead.cpp.obj.d"
-  "C:/Users/cerve/CLionProjects/POS2022-pong/FileHandling/FileWrite.cpp" "CMakeFiles/POS2022_pong.dir/FileHandling/FileWrite.cpp.obj" "gcc" "CMakeFiles/POS2022_pong.dir/FileHandling/FileWrite.cpp.obj.d"
-  "C:/Users/cerve/CLionProjects/POS2022-pong/Game/Game.cpp" "CMakeFiles/POS2022_pong.dir/Game/Game.cpp.obj" "gcc" "CMakeFiles/POS2022_pong.dir/Game/Game.cpp.obj.d"
-  "C:/Users/cerve/CLionProjects/POS2022-pong/Platform/Platform.cpp" "CMakeFiles/POS2022_pong.dir/Platform/Platform.cpp.obj" "gcc" "CMakeFiles/POS2022_pong.dir/Platform/Platform.cpp.obj.d"
-  "C:/Users/cerve/CLionProjects/POS2022-pong/main.cpp" "CMakeFiles/POS2022_pong.dir/main.cpp.obj" "gcc" "CMakeFiles/POS2022_pong.dir/main.cpp.obj.d"
-  "C:/Users/cerve/CLionProjects/POS2022-pong/server.cpp" "CMakeFiles/POS2022_pong.dir/server.cpp.obj" "gcc" "CMakeFiles/POS2022_pong.dir/server.cpp.obj.d"
+  "C:/Users/samuel.pekar/Desktop/skola/POS/POS2022-pong/Ball/Ball.cpp" "CMakeFiles/POS2022_pong.dir/Ball/Ball.cpp.obj" "gcc" "CMakeFiles/POS2022_pong.dir/Ball/Ball.cpp.obj.d"
+  "C:/Users/samuel.pekar/Desktop/skola/POS/POS2022-pong/FileHandling/FileRead.cpp" "CMakeFiles/POS2022_pong.dir/FileHandling/FileRead.cpp.obj" "gcc" "CMakeFiles/POS2022_pong.dir/FileHandling/FileRead.cpp.obj.d"
+  "C:/Users/samuel.pekar/Desktop/skola/POS/POS2022-pong/FileHandling/FileWrite.cpp" "CMakeFiles/POS2022_pong.dir/FileHandling/FileWrite.cpp.obj" "gcc" "CMakeFiles/POS2022_pong.dir/FileHandling/FileWrite.cpp.obj.d"
+  "C:/Users/samuel.pekar/Desktop/skola/POS/POS2022-pong/Game/Game.cpp" "CMakeFiles/POS2022_pong.dir/Game/Game.cpp.obj" "gcc" "CMakeFiles/POS2022_pong.dir/Game/Game.cpp.obj.d"
+  "C:/Users/samuel.pekar/Desktop/skola/POS/POS2022-pong/Platform/Platform.cpp" "CMakeFiles/POS2022_pong.dir/Platform/Platform.cpp.obj" "gcc" "CMakeFiles/POS2022_pong.dir/Platform/Platform.cpp.obj.d"
+  "C:/Users/samuel.pekar/Desktop/skola/POS/POS2022-pong/client/client.cpp" "CMakeFiles/POS2022_pong.dir/client/client.cpp.obj" "gcc" "CMakeFiles/POS2022_pong.dir/client/client.cpp.obj.d"
+  "C:/Users/samuel.pekar/Desktop/skola/POS/POS2022-pong/main.cpp" "CMakeFiles/POS2022_pong.dir/main.cpp.obj" "gcc" "CMakeFiles/POS2022_pong.dir/main.cpp.obj.d"
+  "C:/Users/samuel.pekar/Desktop/skola/POS/POS2022-pong/server/server.cpp" "CMakeFiles/POS2022_pong.dir/server/server.cpp.obj" "gcc" "CMakeFiles/POS2022_pong.dir/server/server.cpp.obj.d"
   )
 
 # Targets to which this target links.
